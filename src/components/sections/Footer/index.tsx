b36e8c99da56503ff84fc5ff0c467169e9a0e7fc
Stackbit Code Editor <projects@stackbit.com>
2022-06-18 02:47:20 +0000

    index.tsx: changed

--- src/components/sections/Footer/index.tsx
+++ src/components/sections/Footer/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StackbitObjectId, getObjectId, toFieldPath } from '@stackbit/annotations';
-import {Footer} from 'types';
+import {Footer as Footer} from 'types';
 
 import MuiBox from '@mui/material/Box';
 import MuiTypography from '@mui/material/Typography';
